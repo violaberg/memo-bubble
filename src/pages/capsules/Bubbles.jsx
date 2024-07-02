0d0cb4201cbaf32cc63b491afcb05140a5bc8e96
vasileios20 <vasilis_tsim@outlook.com>
2024-07-02 19:45:10 +0100

    chore: Update Bubbles.jsx to include link to open(redirect to) a capsule

--- src/pages/capsules/Bubbles.jsx
+++ src/pages/capsules/Bubbles.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 const Bubbles = ({ dataResults }) => {
   const data = dataResults.results || [];
@@ -10,14 +11,17 @@ const Bubbles = ({ dataResults }) => {
     <Container>
       <div>Lots of Bubbles will be rendered here</div>
       <div>
+        Create a new capsule
         {data.map((capsule, index) => (
-          <div key={index}>
-            <h2>{capsule.title}</h2>
-            <p>{capsule.message}</p>
-            <p>
-              <strong>Created On: {capsule.created_on}</strong>
-            </p>
-          </div>
+          <Link to={`capsules/${capsule.id}`}>
+            <div key={index}>
+              <h2>{capsule.title}</h2>
+              <p>{capsule.message}</p>
+              <p>
+                <strong>Created On: {capsule.created_on}</strong>
+              </p>
+            </div>
+          </Link>
         ))}
       </div>
     </Container>
