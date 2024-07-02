8cf445666e063b43f3b369f143e552db2e3a542c
vasileios20 <vasilis_tsim@outlook.com>
2024-07-02 19:32:11 +0100

    chore: Update Bubbles and CapsulePage components

--- src/pages/capsules/Bubbles.jsx
+++ src/pages/capsules/Bubbles.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
 
-const Bubbles = () => {
+const Bubbles = ({ dataResults }) => {
+  const data = dataResults.results || [];
+  console.log('data', data);
+  console.log('dataResults', dataResults);
+
   return (
-    <div>Lots of Bubbles will be rendered here</div>
-  )
-}
+    <Container>
+      <div>Lots of Bubbles will be rendered here</div>
+      <div>
+        {data.map((capsule, index) => (
+          <div key={index}>
+            <h2>{capsule.title}</h2>
+            <p>{capsule.message}</p>
+            <p>
+              <strong>Created On: {capsule.created_on}</strong>
+            </p>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
+};
 
-export default Bubbles
+export default Bubbles;
