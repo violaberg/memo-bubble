6814a59d2308267909c4e6f3f9ddd70649295b86
unknown <viola.bergere@gmail.com>
2024-07-09 09:01:09 +0100

    Update footer styling, add pattern background, and reorganize component files
    - Update footer styling for improved layout and design
    - Add a patterned background to the footer
    - Move footer component files into the 'components' folder
    - Update import paths to reflect new component locations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import InfoLinks from '../pages/footer/InfoLinks';
-import LegalLinks from '../pages/footer/LegalLinks';
-import DeveloperDetails from '../pages/footer/DeveloperDetails';
+import InfoLinks from './footer/InfoLinks';
+import LegalLinks from './footer/LegalLinks';
+import DeveloperDetails from './footer/DeveloperDetails';
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
@@ -23,7 +23,7 @@ const Footer = () => {
                 <Row>
                     <Col>
                         <p className={styles.copyRight}>
-                            &copy; 2024 All rights reserved <span>MEMO BUBBLE</span>
+                            &copy; 2024 All rights reserved <span className={styles.brandName}>MEMO BUBBLE</span>
                         </p>
                     </Col>
                 </Row>
--- src/components/footer/InfoLinks.jsx
+++ src/components/footer/InfoLinks.jsx
@@ -4,7 +4,7 @@ import styles from "../../styles/Footer.module.css";
 const InfoLinks = () => {
     return (
         <div className={styles.infoLinks}>
-            <h5>Contact & Info</h5>
+            <h3 className={styles.footerHeader}>Contact & Info</h3>
             <ul>
                 <li><a href="/contac">About</a></li>
                 <li><a href="/about">Contact</a></li>
