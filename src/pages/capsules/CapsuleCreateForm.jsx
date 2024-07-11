9d9ad228b4d9e3f2f703ada96c362d0a08c3ee7e
vasileios20 <vasilis_tsim@outlook.com>
2024-07-11 13:41:48 +0100

    Add gemini model to CapsuleCreateForm component
    - Commented out code to test gemini
    -Add function to restrict 15 requests per minute (if page refresh counter starts over)

--- src/pages/capsules/CapsuleCreateForm.jsx
+++ src/pages/capsules/CapsuleCreateForm.jsx
@@ -22,15 +22,62 @@ function CapsuleCreateForm() {
     images: '',
     uploaded_images: [],
     uploaded_videos: [],
+    gemini_message: '',
   });
 
-  const { title, message, release_date, uploaded_images, uploaded_videos } =
-    capsuleData;
+  const {
+    title,
+    message,
+    release_date,
+    uploaded_images,
+    uploaded_videos,
+    gemini_message,
+  } = capsuleData;
   const [uploadProgress, setUploadProgress] = useState(0);
   const [errors, setErrors] = useState({});
   const imageInput = useRef(null);
   const videoInput = useRef(null);
   const history = useHistory();
+
+  const { GoogleGenerativeAI } = require('@google/generative-ai');
+
+  // Access your API key as an environment variable (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
+
+  async function run() {
+    // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+    const model = genAI.getGenerativeModel({
+      model: 'gemini-1.5-flash',
+      generation_config: { response_mime_type: 'application/json' },
+    });
+
+    const prompt = gemini_message;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    return text;
+  }
+
+  const [executionCount, setExecutionCount] = React.useState(0);
+  const [lastExecutionTime, setLastExecutionTime] = React.useState(null);
+
+  const executeRun = async () => {
+    const currentTime = new Date().getTime();
+    if (
+      executionCount < 15 ||
+      (lastExecutionTime && currentTime - lastExecutionTime > 60000)
+    ) {
+      setExecutionCount(executionCount + 1);
+      setLastExecutionTime(currentTime);
+      console.log('Execution count:', executionCount);
+      console.log('Last execution time:', lastExecutionTime);
+      await run();
+    } else {
+      console.log('Execution limit exceeded');
+    }
+  };
 
   useEffect(() => {
     return () => {
@@ -89,96 +136,99 @@ function CapsuleCreateForm() {
     });
   };
 
-  const getPresignedUrl = async (fileName) => {
-    const response = await axiosReq.get(
-      `/generate_presigned_url/?file_name=${fileName}`
-    );
-    return response.data;
-  };
+  // const getPresignedUrl = async (fileName) => {
+  //   const response = await axiosReq.get(
+  //     `/generate_presigned_url/?file_name=${fileName}`
+  //   );
+  //   return response.data;
+  // };
 
-  const uploadFileToS3 = async (file, presignedUrl) => {
-    const formData = new FormData();
-    Object.keys(presignedUrl.fields).forEach((key) => {
-      formData.append(key, presignedUrl.fields[key]);
-    });
-    formData.append('file', file);
+  // const uploadFileToS3 = async (file, presignedUrl) => {
+  //   const formData = new FormData();
+  //   Object.keys(presignedUrl.fields).forEach((key) => {
+  //     formData.append(key, presignedUrl.fields[key]);
+  //   });
+  //   formData.append('file', file);
 
-    await axios.post(presignedUrl.url, formData, {
-      headers: { 'Content-Type': 'multipart/form-data' },
-      onUploadProgress: (progressEvent) => {
-        const percentCompleted = Math.round(
-          (progressEvent.loaded * 100) / progressEvent.total
-        );
-        setUploadProgress(
-          (prevProgress) =>
-            prevProgress + percentCompleted / uploaded_videos.length
-        );
-      },
-    });
-  };
+  //   await axios.post(presignedUrl.url, formData, {
+  //     headers: { 'Content-Type': 'multipart/form-data' },
+  //     onUploadProgress: (progressEvent) => {
+  //       const percentCompleted = Math.round(
+  //         (progressEvent.loaded * 100) / progressEvent.total
+  //       );
+  //       setUploadProgress(
+  //         (prevProgress) =>
+  //           prevProgress + percentCompleted / uploaded_videos.length
+  //       );
+  //     },
+  //   });
+  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setUploadProgress(0);
 
-    const uploadedImageUrls = [];
-    for (const { file } of uploaded_images) {
-      const presignedUrl = await getPresignedUrl(file.name);
-      await uploadFileToS3(file, presignedUrl);
-      uploadedImageUrls.push(presignedUrl.url + presignedUrl.fields.key);
-    }
+    // const uploadedImageUrls = [];
+    // for (const { file } of uploaded_images) {
+    //   const presignedUrl = await getPresignedUrl(file.name);
+    //   await uploadFileToS3(file, presignedUrl);
+    //   uploadedImageUrls.push(presignedUrl.url + presignedUrl.fields.key);
+    // }
 
-    const uploadedVideoUrls = [];
-    for (const { file } of uploaded_videos) {
-      const presignedUrl = await getPresignedUrl(file.name);
-      await uploadFileToS3(file, presignedUrl);
-      uploadedVideoUrls.push(presignedUrl.url + presignedUrl.fields.key);
-    }
+    // const uploadedVideoUrls = [];
+    // for (const { file } of uploaded_videos) {
+    //   const presignedUrl = await getPresignedUrl(file.name);
+    //   await uploadFileToS3(file, presignedUrl);
+    //   uploadedVideoUrls.push(presignedUrl.url + presignedUrl.fields.key);
+    // }
 
-    const metadata = {
-      uploaded_images_metadata: uploaded_images.map(
-        ({ date_taken, file }, idx) => ({
-          url: uploadedImageUrls[idx],
-          date_taken: `${date_taken}T12:00:00Z`,
-          gemini_messages: [
-            { message: `Gemini message for image ${file.name}` },
-          ],
-        })
-      ),
-      uploaded_videos_metadata: uploaded_videos.map(
-        ({ date_taken, file }, idx) => ({
-          url: uploadedVideoUrls[idx],
-          date_taken: `${date_taken}T12:00:00Z`,
-          gemini_messages: [
-            { message: `Gemini message for video ${file.name}` },
-          ],
-        })
-      ),
-    };
+    // const metadata = {
+    //   uploaded_images_metadata: uploaded_images.map(
+    //     ({ date_taken, file }, idx) => ({
+    //       url: uploadedImageUrls[idx],
+    //       date_taken: `${date_taken}T12:00:00Z`,
+    //       gemini_messages: [
+    //         { message: `Gemini message for image ${file.name}` },
+    //       ],
+    //     })
+    //   ),
+    //   uploaded_videos_metadata: uploaded_videos.map(
+    //     ({ date_taken, file }, idx) => ({
+    //       url: uploadedVideoUrls[idx],
+    //       date_taken: `${date_taken}T12:00:00Z`,
+    //       gemini_messages: [
+    //         { message: `Gemini message for video ${file.name}` },
+    //       ],
+    //     })
+    //   ),
+    // };
 
     const formData = new FormData();
     formData.append('title', title);
     formData.append('message', message);
     formData.append('release_date', release_date);
-    formData.append(
-      'uploaded_images_metadata',
-      JSON.stringify(metadata.uploaded_images_metadata)
-    );
-    formData.append(
-      'uploaded_videos_metadata',
-      JSON.stringify(metadata.uploaded_videos_metadata)
-    );
+    formData.append('gemini_message', run());
+    // formData.append(
+    //   'uploaded_images_metadata',
+    //   JSON.stringify(metadata.uploaded_images_metadata)
+    // );
+    // formData.append(
+    //   'uploaded_videos_metadata',
+    //   JSON.stringify(metadata.uploaded_videos_metadata)
+    // );
 
     try {
-      const { data } = await axiosReq.post('/capsules/', formData, {
-        onUploadProgress: (progressEvent) => {
-          const percentCompleted = Math.round(
-            (progressEvent.loaded * 100) / progressEvent.total
-          );
-          setUploadProgress(percentCompleted);
-        },
-      });
-      history.push(`/capsules/${data.id}`);
+      // const { data } = await axiosReq.post('/capsules/', formData, {
+      //   onUploadProgress: (progressEvent) => {
+      //     const percentCompleted = Math.round(
+      //       (progressEvent.loaded * 100) / progressEvent.total
+      //     );
+      //     setUploadProgress(percentCompleted);
+      //   },
+      // });
+      // history.push(`/capsules/${data.id}`);
+      console.log('formData', formData.get('gemini_message'));
+      executeRun();
     } catch (err) {
       setErrors(err.response?.data);
     }
@@ -206,6 +256,18 @@ function CapsuleCreateForm() {
           videoInput={videoInput}
           handleDateChange={handleDateChange}
         />
+        <Form.Group>
+          <Form.Label htmlFor='release_date' style={{ color: 'black' }}>
+            Gemini message input
+          </Form.Label>
+          <Form.Control
+            type='text'
+            name='gemini_message'
+            value={capsuleData.gemini_message}
+            onChange={handleChange}
+            style={{ height: '100px', color: 'black' }}
+          />
+        </Form.Group>
         {uploadProgress > 0 && (
           <div className='progress'>
             <div
