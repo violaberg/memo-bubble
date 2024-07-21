2a4eb16f28f5a0106d1d78caffd942d88fa5a9c4
unknown <viola.bergere@gmail.com>
2024-07-21 19:52:17 +0100

    style: Adjust missed borders and margin for sign in and sign up forms

--- src/pages/auth/SignUpForm.jsx
+++ src/pages/auth/SignUpForm.jsx
@@ -62,105 +62,107 @@ const SignUpForm = () => {
   };
 
   return (
-    <Row className={`${styles.SignInUpForm} m-4`}>
-      <Col className='my-auto py-2 p-md-2' md={6}>
-        <Container className={`${styles.FormContent} text-center`}>
-          <h1 className={styles.Header}>Sign up</h1>
+    <Container className='py-3'>
+      <Row className={`${styles.SignInUpForm} mx-auto m-4`}>
+        <Col className='my-auto py-2 p-md-2' md={6}>
+          <Container className={`${styles.FormContent} text-center`}>
+            <h1 className={styles.Header}>Sign up</h1>
 
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId='username'>
-              <Form.Label className='d-none'>username</Form.Label>
-              <Form.Control
-                className={`${styles.Input} mx-auto shadow`}
-                type='text'
-                placeholder='Username'
-                name='username'
-                value={username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert variant='warning' key={idx}>
-                {message}
-              </Alert>
-            ))}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId='username'>
+                <Form.Label className='d-none'>username</Form.Label>
+                <Form.Control
+                  className={`${styles.Input} mx-auto shadow`}
+                  type='text'
+                  placeholder='Username'
+                  name='username'
+                  value={username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.username?.map((message, idx) => (
+                <Alert variant='warning' key={idx}>
+                  {message}
+                </Alert>
+              ))}
 
-            <Form.Group controlId='email'>
-              <Form.Label className='d-none'>email</Form.Label>
-              <Form.Control
-                className={`${styles.Input} mx-auto shadow`}
-                type='email'
-                placeholder='Email'
-                name='email'
-                value={email}
-                onChange={handleChange}
-              />
-            </Form.Group>
+              <Form.Group controlId='email'>
+                <Form.Label className='d-none'>email</Form.Label>
+                <Form.Control
+                  className={`${styles.Input} mx-auto shadow`}
+                  type='email'
+                  placeholder='Email'
+                  name='email'
+                  value={email}
+                  onChange={handleChange}
+                />
+              </Form.Group>
 
-            <Form.Group controlId='password1'>
-              <Form.Label className='d-none'>Password</Form.Label>
-              <Form.Control
-                className={`${styles.Input} mx-auto shadow`}
-                type='password'
-                placeholder='Password'
-                name='password1'
-                value={password1}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password1?.map((message, idx) => (
-              <Alert variant='warning' key={idx}>
-                {message}
-              </Alert>
-            ))}
-            <Form.Group controlId='password2'>
-              <Form.Label className='d-none'>Confirm password</Form.Label>
-              <Form.Control
-                className={`${styles.Input} mx-auto shadow`}
-                type='password'
-                placeholder='Confirm password'
-                name='password2'
-                value={password2}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password2?.map((message, idx) => (
-              <Alert variant='warning' key={idx}>
-                {message}
-              </Alert>
-            ))}
+              <Form.Group controlId='password1'>
+                <Form.Label className='d-none'>Password</Form.Label>
+                <Form.Control
+                  className={`${styles.Input} mx-auto shadow`}
+                  type='password'
+                  placeholder='Password'
+                  name='password1'
+                  value={password1}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password1?.map((message, idx) => (
+                <Alert variant='warning' key={idx}>
+                  {message}
+                </Alert>
+              ))}
+              <Form.Group controlId='password2'>
+                <Form.Label className='d-none'>Confirm password</Form.Label>
+                <Form.Control
+                  className={`${styles.Input} mx-auto shadow`}
+                  type='password'
+                  placeholder='Confirm password'
+                  name='password2'
+                  value={password2}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password2?.map((message, idx) => (
+                <Alert variant='warning' key={idx}>
+                  {message}
+                </Alert>
+              ))}
 
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.ButtonSecondary} shadow`}
-              type='submit'
-            >
-              Sign up
-            </Button>
-            {errors.non_field_errors?.map((message, idx) => (
-              <Alert variant='warning' key={idx} className='mt-3'>
-                {message}
-              </Alert>
-            ))}
-          </Form>
-        </Container>
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.ButtonSecondary} shadow`}
+                type='submit'
+              >
+                Sign up
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <Alert variant='warning' key={idx} className='mt-3'>
+                  {message}
+                </Alert>
+              ))}
+            </Form>
+          </Container>
 
-        <Container className={`shadow my-3 w-75 ${appStyles.Content}`}>
-          <Link className={styles.Link} to='/signin'>
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${styles.FillerImage} shadow`}
-          src={signUp}
-          alt='Colourful neon bubbles'
-        />
-      </Col>
-    </Row>
+          <Container className={`shadow my-3 w-75 ${styles.SignInUpLink}`}>
+            <Link className={styles.Link} to='/signin'>
+              Already have an account? <span>Sign in</span>
+            </Link>
+          </Container>
+        </Col>
+        <Col
+          md={6}
+          className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+        >
+          <Image
+            className={`${styles.FillerImage} shadow`}
+            src={signUp}
+            alt='Colourful neon bubbles'
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
